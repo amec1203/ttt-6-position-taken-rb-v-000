55cec1d85aa1c766eba6a85b8cc31d18a87c81d0
Angela Campion <campion.angela@gmail.com>
2018-04-09 15:14:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,1 @@
-boardX = [" ", "X", " ", " ", " ", " ", "O", " ", " "]
-indexX = 
-
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else
-    true
-  end
-end
